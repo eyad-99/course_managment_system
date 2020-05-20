2e37f8a97c15410740b6e97d7ad038091d564e1f
eyad-99 <62146776+eyad-99@users.noreply.github.com>
2020-05-20 07:37:11 -0700

    database

---
+++ database/migrations/2020_05_20_142051_course_student.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CourseStudent extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('course_student', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('course_id');
+            $table->integer('student_id');
+            $table->integer('grade')->nullable();
+             $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('course_student');
+    }
+}
